23e18d279476a36ee64dfc2fafcc262e05052501
Dmytro Martynenko <dmitrom89@gmail.com>
2022-06-26 00:57:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+class TodoList {
+    constructor(url) {
+        this.url = url;
+        this.$input = $('#input');
+        this.$list = $('#list');
+
+        $('#todo').on('click', (e) => {
+            switch (e.target.dataset.action) {
+                case 'status':
+                    const id = $(e.target).closest('li').data('id');
+                    const status = $(e.target).closest('li').data('status');
+                    this.changeStatus(id, status);
+                    break;
+                case 'delete':
+                    const conf = confirm('Are you sure?');
+                    if (!conf) {
+                        return false;
+                    }
+
+                    this.removeTodo($(e.target).closest('li').data('id'));
+                    break;
+                case 'create':
+                    if (this.$input.val().length > 0) {
+                        this.addTodo(this.$input.val());
+                    }
+                    break;
+            }
+        });
+    }
+
+    addTodo(task) {
+        const data = JSON.stringify({
+            task: task,
+            complited: false
+        });
+
+        this.ajaxCall(this.url, 'POST', data, (res) => {
+            const taskNode = this.createLiItem(res);
+            this.$list.append(taskNode);
+            this.$input.val('');
+        });
+    }
+
+    removeTodo(id) {
+        this.ajaxCall(this.url + `/${id}`, 'DELETE', '', (res) => {
+            $(`[data-id="${id}"]`).remove();
+        })
+    }
+
+    getTodos() {
+        this.ajaxCall(this.url, null, null, (res) => {
+           this.render(res);
+        });
+    }
+
+    changeStatus(id, status) {
+        this.ajaxCall(this.url + `/${id}`, 'PATCH', JSON.stringify({complited: !status}), (res) => {
+            $(`[data-id="${id}"]`).toggleClass('done');
+        })
+    }
+
+    render(todos) {
+        let lis = '';
+        for (const el of todos) {
+            if (!el) {
+                return;
+            }
+
+            lis += this.createLiItem(el);
+        }
+
+        this.$list.html(lis);
+    }
+
+    createLiItem(task) {
+        const item = `<li class="${task.complited ? 'done' : ''}" data-id="${task.id}" data-status="${task.complited}">
+                    ${task.id}. ${task.task}
+                    <div class="btns">
+                        <button class="btn btn-secondary btn-sm set-status" data-action="status">Change status</button> 
+                        <button class="btn btn-danger btn-sm delete-task" data-action="delete">Delete</button>
+                    </div>
+                    </li>`;
+
+        return item;
+    }
+
+    ajaxCall(url, type, data, callback) {
+        const params = {
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+        };
+
+        if (type) {
+            params.type = type;
+        }
+
+        if (data) {
+            params.data = data;
+        }
+
+        $.ajax(params)
+            .done(result => callback(result))
+            .fail(err => alert(err.statusText));
+    }
+}
+
+const todo = new TodoList('http://localhost:3000/todos');
+todo.getTodos();
